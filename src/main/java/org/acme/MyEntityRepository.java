e79b1921161f1dd10bc328d45ae745225b11e568
x80486 <gfd2zs93@duck.com>
2024-09-27 12:26:33 -0400

    feat: make use of transactional annotation

--- src/main/java/org/acme/MyEntityRepository.java
+++ src/main/java/org/acme/MyEntityRepository.java
@@ -3,55 +3,31 @@ package org.acme;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Transactional
 @ApplicationScoped
 @RequiredArgsConstructor(onConstructor_ = @Inject)
 public class MyEntityRepository {
   private final EntityManager entityManager;
 
   public void delete(final MyEntity entity) {
-    var transaction = entityManager.getTransaction();
-    try {
-      transaction.begin();
-      entityManager.remove(entity);
-      transaction.commit();
-    } catch (Exception e) {
-      transaction.rollback();
-    }
+    entityManager.remove(entity);
   }
 
   public Optional<MyEntity> findByID(final UUID primaryKey) {
-    try {
-      return Optional.ofNullable(entityManager.find(MyEntity.class, primaryKey));
-    } catch (Exception e) {
-      /* Nevermind */
-    }
-    return Optional.empty();
+    return Optional.ofNullable(entityManager.find(MyEntity.class, primaryKey));
   }
 
   public void save(final MyEntity entity) {
-    var transaction = entityManager.getTransaction();
-    try {
-      transaction.begin();
-      entityManager.persist(entity);
-      transaction.commit();
-    } catch (Exception e) {
-      transaction.rollback();
-    }
+    entityManager.persist(entity);
   }
 
   public void update(final MyEntity entity) {
-    var transaction = entityManager.getTransaction();
-    try {
-      transaction.begin();
-      entityManager.merge(entity);
-      transaction.commit();
-    } catch (Exception e) {
-      transaction.rollback();
-    }
+    entityManager.merge(entity);
   }
 }
